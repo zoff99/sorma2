208c5ed2f4be209bb4c9a25600961d5343b65a35
zoff99 <zoff99@users.noreply.github.com>
2025-12-05 18:20:13 +0100

    add linux gradle example

---
+++ example_gradle_linux/app/src/test/java/org/example/sorma2/AppTest.java
@@ -0,0 +1,10 @@
+package org.example.sorma2;
+ 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test public void test01() {
+        App classUnderTest = new App();
+    }
+}
